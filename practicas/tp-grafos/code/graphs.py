c74fb222c045a3e4ce54abb2f193848bc0842728
EnzoJP <enzoopalau@gmail.com>
2023-05-10 16:59:36 -0300

    subo prim,kruskal y dijkstra

--- practicas/tp-grafos/code/graphs.py
+++ practicas/tp-grafos/code/graphs.py
@@ -4,7 +4,7 @@
 """Ejercicio 1"""
 
 def createGraph(listV, listA):
-    #operación crear grafo
+    #operación crear grafo Lista de adyacencia
     #ListV-listaNumeros, ListA-ListaTuplas
     graph=[]
     #creo la lista de adyacencia con los vertices
@@ -236,6 +236,8 @@ def DFS_Visit2(G,V,grayList,whitelist,blackList):
 def countConnections(Grafo):
     #Implementa la operación cantidad de componentes conexas
     verts=[]
+    #cantidad de componentes conexas
+    arist=[] #lista de aristas 
     for each in Grafo:
         verts.append(each[0])
     #veo los vertices
@@ -351,10 +353,17 @@ def bestRoad(graph, start, goal):
                 "path doesn't exist :(")
     return
 
-
+#otra implementacion es asignarle a cada nodo un parent en bfs y luego ver el camino de uno a otro 
+# en el arbol bfs
 
 """Extras"""
 
+def make_set(graph):
+    L=[]
+    for i in range(0,len(graph)):
+        L.append([])
+        L[i].append(graph[i][0])
+    return L
 
 def DFS_Visit_TREE(G,V,grayList,whitelist,blackList,Aristas):
     #saco de los blancos y los pongo en gris
@@ -369,7 +378,300 @@ def DFS_Visit_TREE(G,V,grayList,whitelist,blackList,Aristas):
     blackList.append(V)
 
 
+#Part 3------->
 
+
+def createGraph_Mat(vertices, aristas):
+    #crea la matriz de adyacencia pero SIN los bordes
+    vertices.sort()
+    n = len(vertices)
+    # crear matriz de adyacencia inicializada con ceros
+    matriz = [[0] * n for x in range(n)]
+    # llenar matriz con unos donde haya aristas
+    for u, v,weight in aristas:
+        i = vertices.index(u)
+        j = vertices.index(v)
+        matriz[i][j] = matriz[j][i] = weight   
+    return matriz
+
+def createGraph_Mat_with_edges(vertices, aristas):
+    #crea la matriz de adyacencia pero CON los bordes
+    #agrego los bordes
+    vertices.sort()
+    n = len(vertices)
+    # crear matriz de adyacencia inicializada con ceros
+    matriz = [[0] * n for x in range(n)]
+    # llenar matriz con unos donde haya aristas
+    for u, v,weight in aristas:
+        i = vertices.index(u)
+        j = vertices.index(v)
+        matriz[i][j] = matriz[j][i] = weight
+    for i in range(0,len(matriz)):
+        matriz[i].insert(0,vertices[i])
+    matriz.insert(0,["V"])
+    for each in vertices:
+        matriz[0].append(each)
+ 
+    return matriz
+
+def printMat2(mat):
+    #imprime una matriz hecha con listas que SI tiene los bordes
+    for each in mat:
+        print(each)
+
+def printMat(mat,vertices):
+    #imprime una matriz hecha con listas que NO tiene los bordes
+    print("V",vertices)
+    cont=0
+    for each in mat:
+        cont+=1
+        if cont==len(mat):
+            print(vertices[cont-1],each,"]")
+        else:
+            print(vertices[cont-1],each)
+
+
+
+def PRIM(G):
+    #algoritmo de prim para AACM
+    # Numero de vertices
+    N = len(G)
+    selected_node = [0]*N
+    U = 0
+    selected_node[0] = True
+
+    ListV=[]
+    ListA=[]
+
+    while (U < N - 1): #(O|V|)
+        vert=search_minimun_edge(N,selected_node,G,ListA)
+        selected_node[vert] = True
+        U += 1
+        ListV.append(vert)
+
+    return createGraph(ListV,ListA)
+
+def search_minimun_edge(N,selected_node,G,ListA):
+    minimum = 999999
+    a = 0
+    b = 0
+    for i in range(N):
+            if selected_node[i] is True:
+                for j in range(N):
+                    if selected_node[j] is not True and G[i][j]!=0:  
+                        # not in selected and there is an edge
+                        if minimum > G[i][j]:
+                            minimum = G[i][j]
+                            a = i
+                            b = j
+    print(str(a) + "-" + str(b) + ":" + str(G[a][b]))
+    aux=(a,b,G[a][b])
+    ListA.append(aux)
+    return b
+
+
+
+def KRUSKAL(Grafo):
+    #algoritmo de Kruskal para AACM
+    Arists=[]
+    verts=[]
+    Makeset=[]
+    #veo las aristas, los vertices y la implementacion de make set
+    for i in range(0,len(Grafo)):
+        if i!=0:
+            verts.append(Grafo[0][i])
+            Makeset.append([Grafo[0][i]])
+            for j in range(0,len(Grafo)):
+                if j!=0:
+                    a=Grafo[0][i]
+                    b=Grafo[j][0]
+                    c=Grafo[a+1][b+1]
+                    if c!=0 and (((a,b,c) not in Arists) and ((b,a,c) not in Arists)) :
+                        Arists.append((a,b,c))
+    #ordeno los pesos
+    AS=sort_by_weight(Arists)
+    New_arist=[]
+    for each in AS:
+        a=find_set(each[0],Makeset)
+        b=find_set(each[1],Makeset)
+        if a is not b:
+            New_arist.append(each)
+            Union(a,b,Makeset)
+    print(New_arist)
+    return createGraph(verts,New_arist)
+
+def sort_by_weight(L):
+    #ordena por el peso de la arista
+    weights=[]
+    ArSort=[]
+    for each in L:
+        weights.append(each[2])
+    weights.sort()
+    for i in range(0,len(weights)):
+        for j in range(0,len(L)):
+            if weights[i]==L[j][2]:
+                if L[j] not in ArSort: 
+                    ArSort.append(L[j])
+    return ArSort
+
+def find_set(V,Makeset):
+    #busca el conjunto conexo de un vertice dado
+    for each in Makeset:
+        if V in each:
+            return each
+
+def Union(l1,l2,L):
+    #une los conjuntos conexos
+    i=0
+    for each in L:
+        if l1==each:
+            aux=l1
+        elif l2==each:
+            index=i
+        i+=1
+    for each in aux:
+        L[index].append(each)
+    L.remove(l1)
+
+#--------------------------
+
+
+def createDirected_G_Mat_with_edges(vertices, aristas):
+    #crea la matriz de adyacencia pero CON los bordes (Grafo Dirijido)
+    #agrego los bordes
+    #crea la matriz de adyacencia pero SIN los bordes
+    vertices.sort()
+    n = len(vertices)
+    # crear matriz de adyacencia inicializada con ceros
+    matriz = [[0] * n for x in range(n)]
+    # llenar matriz con unos donde haya aristas
+    for u, v,weight in aristas:
+        i = vertices.index(u)
+        j = vertices.index(v)
+        matriz[i][j] = weight
+    for i in range(0,len(matriz)):
+        matriz[i].insert(0,vertices[i])
+    matriz.insert(0,["V"])
+    for each in vertices:
+        matriz[0].append(each)
+ 
+    return matriz
+
+
+class Vertex:
+    distance=None
+    parent=None
+    key=None
+        
+
+
+def shortestPath(Grafo, s, v):
+    #determina el algoritmo de 
+    verts=initRelax(Grafo,s)
+    visited=[]
+    queue=minqueue(verts)#ordeno por dist
+    while len(queue)>0:
+        u=queue.pop(0)
+        visited.append(u)
+        for each in adjunt(u.key,Grafo,verts):# lista de adjuntos
+            if each not in visited:
+                relax(u,each,Grafo)
+        queue=minqueue(queue)
+    #bloque para ver S
+    a=None
+    b=None
+    for each in verts:
+        if each.key==s:
+            a=each
+        elif each.key==v:
+            b=each
+    S_Path=parent_path(b,a)
+    return  S_Path
+
+
+
+
+def initRelax(G,s):
+    #veo los vertices
+    verts=[]
+    Nodes=[]
+    for i in range(0,len(G)):
+        if i!=0:
+            verts.append(G[0][i])
+    #Relax inicial
+    for ve in verts:
+        if ve==s:
+            newNode=Vertex()
+            newNode.key=ve
+            newNode.parent=None
+            newNode.distance=0
+            Nodes.append(newNode)
+        else:
+            newNode=Vertex()
+            newNode.key=ve
+            newNode.parent=None
+            newNode.distance=9999#inf
+            Nodes.append(newNode)
+    return Nodes
+
+def minqueue(V):
+    #devulve una queue con los nodos ordenados por distancia
+    q=[0]*len(V)
+    d=[]
+    for each in V:
+        d.append(each.distance)
+    d.sort()
+    for i in range(0,len(V)):
+        for each in V:
+            if d[i]==each.distance and each not in q:
+                q[i]=each
+    return q
+
+def adjunt(v,G,verts):
+    #da una lista de vertices adjuntos a el
+    adj=[]
+    aux=[]
+    for i in range(len(G)):
+        if i!=0:
+            if G[i][0]==v:
+                for j in range(0,len(G[i])):
+                    if G[i][j]!=0 and j!=0:
+                        aux.append(G[0][j])
+    for each in verts:
+        if each.key in aux:
+            adj.append(each)
+    return adj
+
+def relax(u,v,G):
+    #relaja el vertice actualizando su parent y su distancia
+    if v.distance > (u.distance + calculeweight(u.key,v.key,G)):
+        v.distance = u.distance + calculeweight(u.key,v.key,G)
+        v.parent= u
+
+
+def calculeweight(u,v,G):
+    #calcula el peso de una arista
+    a=G[0].index(v)
+    a=a
+    for i in range(0,len(G)):
+        if i!=0 and G[i][0]==u:
+            return G[i][a]
+
+def parent_path(v,s):
+    #calcula el camino mas corto mirando los parent
+    L=[]
+    L.insert(0,v.key)
+    aux=True
+    while aux!=False:
+        if v.parent is not None:
+            v=v.parent
+            if v.key==s.key:
+                aux=False
+        else:
+            return None
+        L.append(v.key)
+        #L.reverse()
+    return L
 
 
 
--- practicas/tp-grafos/code/main.py
+++ practicas/tp-grafos/code/main.py
@@ -3,14 +3,27 @@ from graphs import *
 
 
 ListV=[0,1,2,3,4,5,6,7]
-ListA=[(0,1),(0,2),(1,2),(1,3),(1,5),(2,5),(2,6),(2,4),(3,5),(3,7),(4,6),(4,7),(5,7),(6,7)]
+ListA=[(0,1,3),(0,2,4),(1,2,2),(1,3,3),(1,5,1),(2,5,8),(2,6,1),(2,4,2),(3,5,5),(3,7,2),(4,6,7),(4,7,1),(5,7,2),(6,7,1),(1,0,3),(7,5,1)]
 
 
+#G=createGraph(ListV, ListA)
+#P_List_AD(G)
+#mat1=createGraph_Mat_with_edges(ListV, ListA)
+#mat=createGraph_Mat(ListV, ListA)
 
-G=createGraph(ListV, ListA)
-P_List_AD(G)
+#printMat(mat,ListV)
+#print("-----------")
+#printMat2(mat1)
+
+#PRIM(mat)
+
+#KRUSKAL(mat1)
 
 
+#ListV=["s","t","y","z","x"]
+#ListA=[("s","y",5),("s","t",10),("y","z",2),("z","x",6),("x","z",4),("z","s",7),("t","x",1),("t","y",2),("y","t",3),("y","x",9)]
 
+matD=createDirected_G_Mat_with_edges(ListV, ListA)
+printMat2(matD)
 
-
+print(shortestPath(matD,3,7))
